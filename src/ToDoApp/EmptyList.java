5508c8c4cce8fe7a077bfb8ee37ad5cee9842513
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-08-06 14:04:07 +0200

    ListTasks completed

---
+++ src/ToDoApp/EmptyList.java
@@ -0,0 +1,7 @@
+package ToDoApp;
+
+public class EmptyList {
+  public static void main(String[] args) {
+    
+  }
+}
--- src/ToDoApp/ListTasks.java
+++ src/ToDoApp/ListTasks.java
@@ -1,4 +1,24 @@
 package ToDoApp;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ListTasks {
+  public static void main(String[] args) {
+    try {
+      Path filePath =
+          Paths.get("C:\\Users\\anhen\\Documents\\greenfox\\pfaniko-todo-app\\Tasks.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      int lineCounter = 1;
+      for (String line : lines) {
+        System.out.println(lineCounter + " - " + line);
+        lineCounter++;
+      }
+    } catch (IOException e) {
+      System.out.println("No such file exists");
+    }
+  }
 }
