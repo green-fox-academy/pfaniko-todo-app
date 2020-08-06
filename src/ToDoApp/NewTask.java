71d93b08b200a61ed8fe738f82fd36fbe9eb8122
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-08-06 17:06:33 +0200

    New Task - append solution added

--- src/ToDoApp/NewTask.java
+++ src/ToDoApp/NewTask.java
@@ -2,19 +2,16 @@ package ToDoApp;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.nio.file.StandardOpenOption;
 
 public class NewTask {
   public void newTask(String string) {
     try {
       Path filePath =
           Paths.get("C:\\Users\\anhen\\Documents\\greenfox\\pfaniko-todo-app\\Tasks.txt");
-      List<String> lines = Files.readAllLines(filePath);
-      lines.add(string);
-      Files.write(filePath, lines);
+      Files.write(filePath, string.getBytes(), StandardOpenOption.APPEND);
     } catch (IOException e) {
       e.printStackTrace();
       System.out.println("File not found");
