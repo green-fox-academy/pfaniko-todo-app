2e51c962143f3f62a9de6c89f984a5306e7d338a
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-08-07 23:14:22 +0200

    ToDoMain - enhancement of command calling

--- src/ToDoApp/ToDoMain.java
+++ src/ToDoApp/ToDoMain.java
@@ -1,49 +1,40 @@
 package ToDoApp;
 
-import java.util.Arrays;
-
 public class ToDoMain {
   public static void main(String[] args) {
     if (args.length == 0) {
       PrintUsage.printUsage();
-    } else {
-      for (String arg : args) {
-        if (arg.equals("-l")) {
-          ListTasks.listTasks();
-        } else if (arg.equals("-a")) {
-          try {
-            int indexArg = Arrays.asList(args).indexOf(arg);
-            String nextArg = args[indexArg + 1];
-            NewTask.newTask(nextArg);
-          } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Unable to add: no task provided");
-          }
-        } else if (arg.equals("-r")) {
-          try {
-            int indexArg = Arrays.asList(args).indexOf(arg) + 1;
-            int index = Integer.parseInt(args[indexArg]);
-            RemoveTask.removeTask(index);
-          } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Unable to remove: no index provided");
-          } catch (NumberFormatException e) {
-            System.out.println("Unable to remove: index is not a number");
-          } catch (IndexOutOfBoundsException e) {
-            System.out.println("Unable to remove: index is out of bound");
-          }
-        } else if (arg.equals("-c")) {
-          try {
-            int indexArg = Arrays.asList(args).indexOf(arg) + 1;
-            int index = Integer.parseInt(args[indexArg]);
-            CheckTask.checkTask(index);
-          } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Unable to check: index is out of bound");
-          } catch (IndexOutOfBoundsException e) {
-            System.out.println("Unable to check: index is out of bound");
-          } catch (NumberFormatException e) {
-            System.out.println("Unable to check: index is not a number");
-          }
-        }
+    } else if (args[0].equals("-l")) {
+      ListTasks.listTasks();
+    } else if (args[0].equals("-a")) {
+      try {
+        String nextArg = args[1];
+        NewTask.newTask(nextArg);
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("Unable to add: no task provided");
       }
+    } else if (args[0].equals("-r")) {
+      try {
+        RemoveTask.removeTask(Integer.parseInt(args[1]));
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("Unable to remove: no index provided");
+      } catch (NumberFormatException e) {
+        System.out.println("Unable to remove: index is not a number");
+      } catch (IndexOutOfBoundsException e) {
+        System.out.println("Unable to remove: index is out of bound");
+      }
+    } else if (args[0].equals("-c")) {
+      try {
+        CheckTask.checkTask(Integer.parseInt(args[1]));
+      } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("Unable to check: no index provided");
+      } catch (IndexOutOfBoundsException e) {
+        System.out.println("Unable to check: index is out of bound");
+      } catch (NumberFormatException e) {
+        System.out.println("Unable to check: index is not a number");
+      }
+    } else {
+      System.out.println("Unsupported argument");
     }
   }
 }
